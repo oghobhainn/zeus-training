877b3077489d3d7a26c9232879d139a8b00f2482
Xavier Dumortier <xavier@zeusit.eu>
2023-01-30 12:13:55 +0100

    end of chapter 6

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,5 +1,5 @@
 from odoo import fields, models
-
+from datetime import timedelta
 
 class EstateProperty(models.Model):
 	_name = "estate.property"
@@ -8,10 +8,13 @@ class EstateProperty(models.Model):
 	name = fields.Char(string="Title", required=True)
 	description = fields.Text(string="Description")
 	postcode = fields.Char(string="Postcode")
-	date_availability = fields.Date(string="Available From")
+	date_availability = fields.Date(string="Available From",
+									copy=False,
+									default=lambda self: fields.Datetime.today() + timedelta(days=90)
+									)
 	expected_price = fields.Float(string="Expected Price", required=True)
-	selling_price = fields.Float(string="Selling Price")
-	bedrooms = fields.Integer(string="Bedrooms")
+	selling_price = fields.Float(string="Selling Price", readonly=True, copy=False)
+	bedrooms = fields.Integer(string="Bedrooms", default=2)
 	living_area = fields.Integer(string="Living Area (sqm)")
 	facades = fields.Integer(string="Facades")
 	garage = fields.Boolean(string="Garage")
@@ -20,7 +23,19 @@ class EstateProperty(models.Model):
 	garden_orientation = fields.Selection(
 		string='Garden orientation',
 		selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
-
+	active = fields.Boolean(string="Active", default=True)
+	state = fields.Selection(
+		string='State',
+		selection=[
+			('new', 'New'),
+			('offer received', 'Offer Received'),
+			('offer accepted', 'Offer Accepted'),
+			('sold', 'Sold'),
+			('canceled', 'Canceled')],
+		required=True,
+		default='new',
+		copy=False
+	)
 
 
 
