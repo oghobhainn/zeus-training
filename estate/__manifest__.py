e8e018fc487782043098dbe09bf2a8df9f1316b9
Xavier Dumortier <xavier@zeusit.eu>
2023-01-30 10:24:43 +0100

    Estate module exercise files

---
+++ estate/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'estate',
+    'version': '1.0',
+    'summary': 'Test Xavier',
+    'description': "Test Xavier",
+    'website': 'http://www.zeusit.eu',
+    'depends': [],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
+    ],
+    'demo': [],
+    'css': ['static/src/css/crm.css'],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
+
