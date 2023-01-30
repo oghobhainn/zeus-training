5265f376fa285dddddc3af62a2906ebf6bdaca61
Maarten Vandenheede <maarten@zeusit.eu>
2023-01-30 16:10:42 +0100

    Changes for chapter 6 and part of chapter 7

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,22 +1,32 @@
 from odoo import fields, models
 
+from datetime import datetime, date
+from dateutil.relativedelta import relativedelta
+
 class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Estate Property App"
 
-    name= fields.Char(required=True)
-    description = fields.Text()
-    postcode = fields.Char()
-    date_availability = fields.Date()
-    expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
-    living_area = fields.Integer()
-    facades = fields.Integer()
-    garage = fields.Boolean()
-    garden = fields.Boolean()
-    garden_area = fields.Integer()
+    name= fields.Char("Estate property name", required=True)
+    description = fields.Text("Description")
+    postcode = fields.Char("Postcode")
+    date_availability = fields.Date("Availability Date", copy=False, default=fields.Datetime.now() + relativedelta(months=3))
+    expected_price = fields.Float("Expected Price", required=True)
+    selling_price = fields.Float("Selling Price", readonly=True, copy=False)
+    bedrooms = fields.Integer("Bedrooms", default=2)
+    living_area = fields.Integer("Living Area")
+    facades = fields.Integer("Facades")
+    garage = fields.Boolean("Garage")
+    garden = fields.Boolean("Garden")
+    garden_area = fields.Integer("Garden Area")
     garden_orientation = fields.Selection(
-        string='Orientation',
-        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],
+        string="Orientation",
+        selection=[("north", "North"), ("south", "South"), ("east", "East"), ("west", "West")],
         help="Orientation is used to define the orientation")
+
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        string="States",
+        selection=[("new", "New"), ("offer received", "Offer Received"), ("offer accepted", "Offer Accepted"), ("sold", "Sold"), ("canceled", "Canceled")],
+        default="new",
+        help="Choose the state of the Offer")
