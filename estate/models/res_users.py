cba97cfee06fc84e214ef276a56cec7ff9a5e560
Xavier Dumortier <xavier@zeusit.eu>
2023-02-02 17:17:06 +0100

    fix domain and change to "in" instead of using "|" (shorter and easiers to change if another state also needs to be shown)

--- estate/models/res_users.py
+++ estate/models/res_users.py
@@ -3,4 +3,4 @@ from odoo import fields, models
 class ResUsers(models.Model):
     _inherit = "res.users"
 
-    property_ids = fields.One2many("estate.property", "seller_id", string="Properties",  domain="['|', ('state', '=', 'new'), ('state', '=', 'offer received')]")
+    property_ids = fields.One2many("estate.property", "seller_id", string="Properties", domain=[('state', 'in', ['new','offer received'])])
